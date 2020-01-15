1d1618c4e3478588f84a0a40602df1dcae74b0f1
Adrian Lois <1989adrianlois@gmail.com>
2020-01-15 22:23:47 +0100

    Mailbox-Identiy-Delegate.ps1

---
+++ Manage-permissions-recipients-Mailbox-EXO/Mailbox-Identiy-Delegate.ps1
@@ -0,0 +1,45 @@
+﻿# Install and import module ExchangeOnlineManagement
+Install-Module -Name ExchangeOnlineManagement
+Import-Module -Name ExchangeOnlineManagement
+
+# Connect Exchange Online without MFA
+$UserCredential = Get-Credential
+Connect-ExchangeOnline -Credential $UserCredential -ShowProgress $true
+
+# Connect Exchange Online with MFA (UPN: User Principal Name - user@domain.com)
+Connect-ExchangeOnline -UserPrincipalName "user@domain.com" -ShowProgress $true
+
+# You are logged on to a computer as a user in the SSO domain 
+Connect-EXPOPSSession
+# New-ExoPSSession
+
+# Variables
+$MailboxIdentity = "Name"
+$MailboxDelegate = 'user1','user2','user3','user4'
+
+ForEach ($User in $MailboxDelegate) {
+
+    # Full Access
+    Add-MailboxPermission `
+        -Identity $MailboxIdentity `
+        -User $User `
+        -AccessRights FullAccess `
+        -InheritanceType All `
+        -Confirm:$false
+
+    # Send As
+    Add-RecipientPermission `
+        -Identity $MailboxIdentity `
+        -Trustee $User `
+        -AccessRights SendAs `
+        -Confirm:$false
+    
+    # Send on Behalf
+    Set-Mailbox "$MailboxIdentity" `
+        -GrantSendOnBehalfTo @{add="$User"} `
+        -Confirm:$false
+}
+
+# Replace existing delegates: <DelegateIdentity> or "<DelegateIdentity1>","<DelegateIdentity2>",...
+# Add or remove delegates without affecting other delegates: @{Add="\<value1\>","\<value2\>"...; Remove="\<value1\>","\<value2\>"...}
+# Remove all delegates: Use the value $null.
