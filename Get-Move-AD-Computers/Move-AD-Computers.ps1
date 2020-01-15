56d22b3db5664371b1c4ee9afd9411509278a322
Adrian Lois <1989adrianlois@gmail.com>
2020-01-15 22:23:15 +0100

    Move-AD-Computers.ps1

---
+++ Get-Move-AD-Computers/Move-AD-Computers.ps1
@@ -0,0 +1,26 @@
+﻿# Load Active Directory module
+Import-Module ActiveDirectory
+
+# LoadBox File Prompt: Select CSV file
+# Comma separated values and UTF-8 encoding
+New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') }
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    Filter = 'Documents (*.csv)|*.csv'
+}
+$FileImportCSV = $FileBrowser.ShowDialog()
+
+# Import file and start loop
+Import-Csv $FileImportCSV | foreach-object {
+
+# Set variables
+    $Name = $_."Name"
+    $Path = $_."Path"
+    $Description = $_."Description"
+
+# Description Computer Object AD
+    Get-ADComputer -Identity "$Name" | Move-ADObject -TargetPath "$Path"
+
+# Move Computer Object AD
+    Get-ADComputer -Identity "$Name" | Set-ADObject -Description "$Description"
+}
