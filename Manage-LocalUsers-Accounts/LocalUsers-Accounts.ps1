da071cad493e80358bd7faf28080a6d8d3eca4eb
adrianlois <34519511+adrianlois@users.noreply.github.com>
2020-09-30 20:10:15 +0200

    Manage-LocalUsers-Accounts

---
+++ Manage-LocalUsers-Accounts/LocalUsers-Accounts.ps1
@@ -0,0 +1,74 @@
+function Set-NewUserPasswordEncrypt {
+
+    $path = "C:\Passwords" #Set you path
+    $keyFile = New-Object Byte[] 32
+    [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($keyFile)
+    $keyFile | Out-File "$path\KeyFilePasswd.key"
+
+    Read-Host -AsSecureString "Set a Password" | ConvertFrom-SecureString -key (Get-Content "$path\KeyFilePasswd.key") | Set-Content "$path\EncryptPasswd"
+
+    $password = Get-Content "$path\EncryptPasswd" | ConvertTo-SecureString -Key (Get-Content "$path\KeyFilePasswd.key")
+    return $password
+}
+
+function New-AdmUser {
+
+    $passwd = Set-NewUserPasswordEncrypt
+    $adminGroup = (Get-LocalGroup -Name "Administra*s").Name | Select-Object -First 1
+    $admUser = "admuser"
+    $admUserCheck = Get-LocalUser | Where-Object {$_.Name -eq "$userStladmin"} | Select-Object Name
+    $admUserPass = ConvertTo-SecureString -String "$passwd" -AsPlainText -Force
+
+    if ( -not $admUserCheck ) {
+        New-LocalUser $admUser -PasswordNeverExpires -Password $admUserPass
+        Add-LocalGroupMember -Group $adminGroup -Member $admUser
+    }
+}
+
+function Set-AdmUserPasswd {
+
+    $passwd = Set-NewUserPasswordEncrypt
+    $admUser = Get-LocalUser -Name "admuser" | Select-Object Name
+    $admUserPass = ConvertTo-SecureString -String "$passwd" -AsPlainText -Force
+
+    if ( $admUser ) {
+        Set-LocalUser -Name $admUser -PasswordNeverExpires:$True -Password $admUserPass
+    }
+}
+
+function Remove-AdmUserAdminGroup {
+
+    $admUser = Get-LocalUser -Name "admuser" | Select-Object Name
+    $userGroup = (Get-LocalGroup -Name "Us*s").Name | Select-Object -First 1  
+    $adminGroup = (Get-LocalGroup -Name "Administra*s").Name | Select-Object -First 1
+
+    if ( $admUser ) {
+        Add-LocalGroupMember -Group $userGroup -Member $admUser
+        Remove-LocalGroupMember -Group $adminGroup -Member $admUser
+    }
+}
+
+function Remove-AdmUser {
+
+    $admUser = (Get-LocalUser | Where-Object {$_.Name -eq "admuser"}).Name
+
+    if ( $admUser ) {
+        Remove-LocalUser -Name $admUser
+    }
+}
+
+function Disable-AdminDefaultAccount {
+
+    $adminAccount = (Get-LocalUser -Name "Administra*r").Name | Select-Object -First 1
+
+    if ( $adminAccount ) {
+        Disable-LocalUser -Name $adminAccount
+    }
+}
+
+#Set-NewUserPasswordEncrypt
+#New-AdmUser
+#Set-AdmUserPasswd
+#Remove-AdmUserAdminGroup
+#Remove-AdmUser
+#Disable-AdminDefaultAccount
---
+++ Manage-LocalUsers-Accounts/README.md
@@ -0,0 +1,5 @@
+# LocalUsers-Accounts
+#### Sample functions for managing Windows local user accounts
+
+- EncryptPasswd: Encrypted Password file.
+- KeyFilePasswd.key: Key file to read encrypted password.
