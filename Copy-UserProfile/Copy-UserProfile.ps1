29cd4af409fccab5b3de119bdfc37317ceff05ca
adrianlois <34519511+adrianlois@users.noreply.github.com>
2020-10-02 17:23:04 +0200

    Copy-UserProfile

---
+++ Copy-UserProfile/Copy-UserProfile.ps1
@@ -0,0 +1,55 @@
+function Copy-UserProfile {
+
+<#
+    .EXAMPLE
+    C:\PS> Copy-UserProfile -PCSrc "PC1" -PCDst "PC2" -User "adrian"
+#>
+
+    param (
+        [parameter(Mandatory = $true)]
+        [String]
+        $PCSrc,
+        [parameter(Mandatory = $true)]
+        [String]
+        $PCDst,
+        [parameter(Mandatory = $true)]
+        [String]
+        $User
+    )
+
+    $src = "\\$PCSrc\c$\Users\$User\"
+    $dst = "\\$PCDst\c$\Users\$User\"
+    $log = $dst+"CopyUserProfile_$User.log"
+
+    $paths = @(
+        "Desktop",
+        "Downloads",
+        "Pictures",
+        "Favorites",
+        "AppData\Local\MicrosoftEdge",
+        "AppData\Local\Microsoft\Internet Explorer",
+        "AppData\Local\Google\Chrome\User Data",
+        "AppData\Local\Mozilla\Firefox\Profiles"
+    )
+
+    foreach ( $path in $paths ) {
+
+        $srcPath = $src+$path
+        $dstPath = $dst+$path
+        $pathExist = Test-Path -Path $srcPath
+
+        if ( $pathExist -eq $True ) {
+
+            Write-Host "[+] Copying ... $path of $User from $PCSrc -> $PCDst " -ForegroundColor White -BackgroundColor DarkGreen
+            ROBOCOPY "$srcPath" "$dstPath" /MIR /Z /W:5 /LOG+:$log
+        } 
+		else {
+            Write-Host "[-] The $path directory doesn't exist for the user $User in $PCSrc " -ForegroundColor White -BackgroundColor DarkRed
+        }
+
+    }
+    
+    Start-Process $log
+    Start-Sleep 2
+    Remove-Item -Path $log -Force
+}
---
+++ Copy-UserProfile/README.md
@@ -0,0 +1,9 @@
+# Copy-UserProfile
+#### Migrate user profiles
+
+- Copy the most relevant directories from a local or domain user profile to a new computer using ROBOCOPY tool.
+
+Usage example:
+```
+Copy-UserProfile -PCSrc "PC1" -PCDst "PC2" -User "adrian"
+```
