dc4bef6694767c747973b111fe8638be927787dd
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-01-18 19:37:23 +0100

    Get-ADGroups-Inherited

---
+++ 14.Get-ADGroups-Inherited/Get-ADGroups-Inherited.ps1
@@ -0,0 +1,39 @@
+function Get-InheritedGroups {
+
+    param (
+        [Parameter(Mandatory=$True)]
+        [string]$User
+    )
+
+    $grpsAD1 = (Get-ADPrincipalGroupMembership -Identity "$user").SamAccountName 
+    Write-Host "$grpsAD1"
+
+    # TODO: if exist check membership group
+    foreach ($grpAD1 in $grpsAD1) {
+        (Get-ADPrincipalGroupMembership -Identity "$grpAD1").SamAccountName
+
+        Write-Output "----- 1 -----"
+        Write-Output "$grpAD1"
+
+        # TODO: if exist check membership group
+        foreach ($grpAD2 in $grpAD1) {
+            (Get-ADPrincipalGroupMembership -Identity $grpAD2).SamAccountName
+
+            Write-Output "----- 2 -----"
+            Write-Output "    $grpAD1 -> $grpAD2"
+
+            # TODO: if exist check membership group
+            foreach ($grpAD3 in $grpAD2) {
+                (Get-ADPrincipalGroupMembership -Identity $grpAD2).SamAccountName
+
+                Write-Output "----- 3 -----"
+                Write-Output "       $grpAD2 -> $grpAD3"
+
+            }
+        }
+    }
+
+}
+
+# member:1.2.840.113556.1.4.1941 -> Get inherited memebership groups
+# Get-ADGroup -LDAPFilter "(member:1.2.840.113556.1.4.1941:=CN=User IT Operations,OU=Test,OU=Usuarios,OU=Dpt_TIC,DC=stl,DC=loc)" | Select-Object DistinguishedName,Name | Format-Table -AutoSize
---
+++ 14.Get-ADGroups-Inherited/README.md
@@ -0,0 +1,2 @@
+# Get-ADGroups-Inherited
+#### Get up to 3 levels of nested inheritances in Active Directory groups.
