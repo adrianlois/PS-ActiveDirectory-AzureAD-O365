4703b72f8e48f9b6cd320638d557099dd9e109ff
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-01-18 19:54:41 +0100

    Update Manage-AD-ResourcesShared.ps1

--- 16.Manage-AD-ResourcesShared/Manage-AD-ResourcesShared.ps1
+++ 16.Manage-AD-ResourcesShared/Manage-AD-ResourcesShared.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param (
     [Parameter(Mandatory=$True)]
-    [string]$NameGroupAD,
+    [string]$NameGroup,
     [Parameter(Mandatory=$True)]
     [string]$SharedFolder
 )
@@ -12,7 +12,7 @@ $ConfigData = Import-LocalizedData -BaseDirectory "config\" -FileName "data.psd1
 
 function Install-Modules {
 
-    if (-not (((Get-Module -ListAvailable).name -eq "NTFSSecurity") -or ((Get-Module -ListAvailable).name -eq "ActiveDirectory"))) {
+    if ( -not (((Get-Module -ListAvailable).name -eq "NTFSSecurity") -or ((Get-Module -ListAvailable).name -eq "ActiveDirectory")) ) {
 
         Install-Module -Name NTFSSecurity -RequiredVersion 4.2.4 -AllowClobber -Verbose -AcceptLicense -Confirm:$false -Repository PSGallery
         Import-Module -Name ActiveDirectory
@@ -22,10 +22,10 @@ function Install-Modules {
 
 function New-ADGroupShare {
 
-    $NameGroupAD = $PrefixNameGroup + $NameGroup
+    $NameGroupAD = $ConfigData.PrefixNameGroup.ToString() + $NameGroup
     $PathShare = $ConfigData.PathShare.ToString()
     $DescriptionPath = $PathShare + $SharedFolder
-    $PathOUIntercambio = $ConfigData.PathOUIntercambio.ToString()
+    $PathOUPublic = $ConfigData.PathOUPublic.ToString()
     
     <#
     $GroupCheck = Get-ADGroup -LDAPFilter "(SAMAccountName=$NameGroupAD)"
@@ -43,7 +43,7 @@ function New-ADGroupShare {
                 -GroupCategory Security `
                 -GroupScope Global `
                 -DisplayName $NameGroupAD `
-                -Path $PathOUIntercambio `
+                -Path $PathOUPublic `
                 -Description "$DescriptionPath"
 
             Add-ADGroupMember `
@@ -69,13 +69,13 @@ function New-Directory {
 function Set-NTFSAcl {
     
     $NTFSOwner = $ConfigData.NTFSOwner.ToString()
-    $BackupAgent = $ConfigData.BackupAgent.ToString()
+    $BackupGroup = $ConfigData.BackupGroup.ToString()
 
     #Set-NTFSOwner -Path $DescriptionPath -Account $NTFSOwner
     Disable-NTFSAccessInheritance -RemoveInheritedAccessRules -Path $DescriptionPath
     Add-NTFSAccess -Path "$DescriptionPath" -Account $NTFSOwner -AccessRights FullControl -AccessType Allow -AppliesTo ThisFolderSubfoldersAndFiles
-    Add-NTFSAccess -Path "$DescriptionPath" -Account $BackupAgent -AccessRights FullControl -AccessType Allow -AppliesTo ThisFolderSubfoldersAndFiles
-    Add-NTFSAccess -Path "$DescriptionPath" -Account $NameGroup -AccessRights Modify -AccessType Allow -AppliesTo ThisFolderSubfoldersAndFiles
+    Add-NTFSAccess -Path "$DescriptionPath" -Account $BackupGroup -AccessRights FullControl -AccessType Allow -AppliesTo ThisFolderSubfoldersAndFiles
+    Add-NTFSAccess -Path "$DescriptionPath" -Account $NameGroupAD -AccessRights Modify -AccessType Allow -AppliesTo ThisFolderSubfoldersAndFiles
 
-    Write-Host "[+] The permissions assigned successfully for $NameGroup " -ForegroundColor White -BackgroundColor DarkGreen
+    Write-Host "[+] The permissions assigned successfully for $NameGroupAD " -ForegroundColor White -BackgroundColor DarkGreen
 }
