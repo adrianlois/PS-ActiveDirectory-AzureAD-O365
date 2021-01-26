b082114ca3440c86c734c1120f702ee420a619f1
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-01-26 20:45:07 +0100

    Get-Inactive-ADCompUsers.ps1

---
+++ 05.Get-Inactive-AD-Computers/Get-Inactive-ADCompUsers.ps1
@@ -0,0 +1,39 @@
+Import-Module ActiveDirectory
+
+function Get-InactiveADComputers {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [int]$DaysInactive
+        [Parameter(Mandatory)]
+        [string]$ExportPath
+    )
+
+    $Time = (Get-Date).Adddays(-($DaysInactive))
+
+    Get-ADComputer -Filter {LastLogonTimeStamp -lt $Time} -Properties LastLogonTimeStamp | `
+    Select-Object Name,@{Name="Stamp";Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}} | `
+    Export-Csv $ExportPath
+}
+
+function Get-InactiveADUsers {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [int]$DaysInactive
+        [Parameter(Mandatory)]
+        [string]$ExportPath
+    )
+
+    Search-ADAccount -AccountInactive -TimeSpan $DaysInactive | `
+    Sort -Property LastLogonDate | `
+    Format-Table SamAccountName, LastLogonDate -AutoSize | `
+    Export-Csv $ExportPath
+
+    <# another way
+      $Time = (Get-Date).Adddays(-($DaysInactive))
+      Get-ADUser -Filter * -Properties * | Where-Object {$_.LastLogonDate -le $Time} | Select UserPrincipalName | Export-Csv $ExportPath
+    #>
+}
