2419292884ed0ac0d57566b93666a6131ad0589b
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-02-12 21:52:24 +0100

    JoinComputerToDomain.ps1

--- 17.Join-ComputerToDomain/JoinComputerToDomain.ps1
+++ 17.Join-ComputerToDomain/JoinComputerToDomain.ps1
@@ -1,47 +1,47 @@
 function Rename-Computer {
 
     param (
-		[Parameter(Mandatory=$true)]
-		[string]$name
-	)
+    	[Parameter(Mandatory=$true)]
+	[string]$name
+    )
 
     process {
         try {
-			$computer = Get-WmiObject -Class Win32_ComputerSystem
-			$result = $computer.Rename($name)
+	    $computer = Get-WmiObject -Class Win32_ComputerSystem
+ 	    $result = $computer.Rename($name)
 
             switch($result.ReturnValue) {       
                 0 { Write-Host "Computer name has been changed." }
-                5 {
-					Write-Error "You need administrative privileges." 
-					exit
-				}
+                5 { 
+		    Write-Error "You need administrative privileges." 
+		    exit 
+		}
                 default {
-					Write-Host "Error: Return value " $result.ReturnValue
-					exit
+		    Write-Host "Error: Return value " $result.ReturnValue
+	            exit
                 }
             }
         }
-		catch {
-			Write-Host "An error has occurred in the name change operation: " $Error
+	catch {
+	    Write-Host "An error has occurred in the name change operation: " $Error
         }
     }
 }
 
 function Join-ComputerToDomain {
     param ( 
-		[Parameter(Mandatory=$true)]
-		[string]$domain 
-	)
+	[Parameter(Mandatory=$true)]
+	[string]$domain 
+     )
 
     process {
-		try {
+	try {
             $domainCredential = $Host.UI.PromptForCredential("Enter your domain credentials", "Enter your credentials to join the computer to the domain:", "", "NetBiosUserName")
             Add-Computer -DomainName $domain -Credential $domainCredential
         }
         catch {
             Write-Error "An error has occurred in the operation of joining the computer to the domain. " $Error
-        }
+	}
     }
 }
 
