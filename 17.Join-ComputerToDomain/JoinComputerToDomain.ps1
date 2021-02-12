981ab15992d04ba820206dc7f320272da2737a88
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-02-12 21:46:55 +0100

    Join-ComputerToDomain

---
+++ 17.Join-ComputerToDomain/JoinComputerToDomain.ps1
@@ -0,0 +1,51 @@
+function Rename-Computer {
+
+    param (
+		[Parameter(Mandatory=$true)]
+		[string]$name
+	)
+
+    process {
+        try {
+			$computer = Get-WmiObject -Class Win32_ComputerSystem
+			$result = $computer.Rename($name)
+
+            switch($result.ReturnValue) {       
+                0 { Write-Host "Computer name has been changed." }
+                5 {
+					Write-Error "You need administrative privileges." 
+					exit
+				}
+                default {
+					Write-Host "Error: Return value " $result.ReturnValue
+					exit
+                }
+            }
+        }
+		catch {
+			Write-Host "An error has occurred in the name change operation: " $Error
+        }
+    }
+}
+
+function Join-ComputerToDomain {
+    param ( 
+		[Parameter(Mandatory=$true)]
+		[string]$domain 
+	)
+
+    process {
+		try {
+            $domainCredential = $Host.UI.PromptForCredential("Enter your domain credentials", "Enter your credentials to join the computer to the domain:", "", "NetBiosUserName")
+            Add-Computer -DomainName $domain -Credential $domainCredential
+        }
+        catch {
+            Write-Error "An error has occurred in the operation of joining the computer to the domain. " $Error
+        }
+    }
+}
+
+$domain = 'domain.local'
+
+Rename-Computer $name
+Join-ComputerToDomain $domain
---
+++ 17.Join-ComputerToDomain/README.md
@@ -0,0 +1,4 @@
+# Join-ComputerToDomain
+#### Set computer name and join computer to AD domain.
+
+Change the value **$domain** (domain.local)
