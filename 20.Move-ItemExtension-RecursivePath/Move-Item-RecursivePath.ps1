4333e5b18615298ef13474e2c8d5504d961a3d21
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-03-08 15:36:45 +0100

    Move-ItemExtension-RecursivePath

---
+++ 20.Move-ItemExtension-RecursivePath/Move-Item-RecursivePath.ps1
@@ -0,0 +1,48 @@
+﻿Function Move-ItemExtension-RecursivePath {
+<#
+    .PARAMETER SrcPathFiles
+    Indicate the parent root path of the source files to search in subdirectories.
+    .PARAMETER ExtFiles
+    Indicate the extension of the source files.
+    .PARAMETER DestinationFiles
+    Indicate the destination of the files to move.
+    .EXAMPLE
+    C:\PS> Move-Item-RecursivePath -SrcPathFiles "D:\Reports\Files" -ExtFiles "csv" -DestinationFiles "C:\CSVs"
+#>
+
+    param (
+        [parameter(Mandatory = $true)]
+        [String]
+        $SrcPathFiles,
+        [parameter(Mandatory = $true)]
+        [String]
+        $ExtFiles,
+        [parameter(Mandatory = $true)]
+        [String]
+        $DestinationFiles
+    )
+
+    $Files = (Get-ChildItem -Path $SrcPathFiles -Recurse | Where-Object {$_.Extension -eq ".$ExtFiles"}).Fullname
+
+    ForEach-Object {
+        Move-Item -Path $Files -Destination $DestinationFiles -Force
+    }
+
+	$DeleteAll = Read-Host "[?] Delete all subdirectories of"`"$SrcPathFiles"`"? (y/n)"
+	
+    if ( $DeleteAll -eq "y") {
+		$Exclude = Read-Host "[?] Exclude a file or directory? (indicate string/n)"
+		
+			if ( $Exclude -ne "n" ) {
+				Get-ChildItem -Path "$SrcPathFiles" -Exclude "$Exclude" | Remove-Item -Recurse -Force
+				Break
+			}
+		
+		Remove-Item -Path "$SrcPathFiles\*" -Recurse -Force
+	}
+	
+	Get-ChildItem -Path "$DestinationFiles"
+}
+
+# another way: execution in a single line
+# (Get-ChildItem -Path $SrcPathFiles -Recurse | Where-Object {$_.Extension -eq ".$ExtFiles"}).Fullname | Move-Item -Destination $DestinationFiles ; Get-ChildItem -Path $SrcPathFiles -Exclude $Exclude | Remove-Item -Recurse -Force
---
+++ 20.Move-ItemExtension-RecursivePath/README.md
@@ -0,0 +1,9 @@
+# Move-ItemExtension-RecursivePath
+#### Move files based on an extension.
+
+Recursively move files of a given extension from subdirectories to a target root directory.
+
+Parameters:
+- SrcPathFiles: Indicate the parent root path of the source files to search in subdirectories.
+- ExtFiles: Indicate the extension of the source files.
+- DestinationFiles: Indicate the destination of the files to move.
