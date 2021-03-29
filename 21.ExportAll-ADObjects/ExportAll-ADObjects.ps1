f9a2e7514b565a7772bfff64cd208043c681c006
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-03-29 11:25:04 +0200

    ExportAll-ADObjects.ps1

--- 21.ExportAll-ADObjects/ExportAll-ADObjects.ps1
+++ 21.ExportAll-ADObjects/ExportAll-ADObjects.ps1
@@ -1,4 +1,4 @@
-# .\ExportAll-ObjectsAD.ps1 -ADComputers -ADGroups -ADUsers -ExportPath "C:\Users\adrian\Desktop\ExportAD"
+# .\ExportAll-ADObjects.ps1 -ADComputers -ADGroups -ADUsers -ExportPath "C:\Users\adrian\Desktop\ExportAD"
 
 param (
     [switch]$ADComputers=$False,
@@ -9,29 +9,29 @@ param (
 )
 
 function ADComputers {
-    Get-ADComputer -Filter * -Properties * -SearchBase "DC=domain,DC=local" | `
-    Select-Object Name,DNSHostName,IPv4Address,DistinguishedName,Enabled,whenCreated,LastLogonDate,`
+	Get-ADComputer -Filter * -Properties * -SearchBase "DC=domain,DC=local" | `
+	Select-Object Name,DNSHostName,IPv4Address,DistinguishedName,Enabled,whenCreated,LastLogonDate,`
 	OperatingSystem,OperatingSystemVersion,ObjectClass,SID | `
-    Export-Csv $ExportPath'\Computers.csv' -NoTypeInformation -Encoding UTF8
+	Export-Csv $ExportPath'\Computers.csv' -NoTypeInformation -Encoding UTF8
 }
 
 function ADGroups {
-    Get-ADGroup -Filter * -Properties * -SearchBase "DC=domain,DC=local" | `
-    Select-Object Name,Description,info,DistinguishedName,whenCreated,whenChanged,`
+	Get-ADGroup -Filter * -Properties * -SearchBase "DC=domain,DC=local" | `
+	Select-Object Name,Description,info,DistinguishedName,whenCreated,whenChanged,`
 	@{Name='Member';Expression={($_.Member | % {(Get-ADObject $_).Name}) -join ";"}},`
 	@{Name='MemberOf';Expression={($_.MemberOf | % {(Get-ADObject $_).Name}) -join ";"}},`
 	GroupCategory,GroupScope,ObjectClass,SID | `
-    Export-Csv $ExportPath'\Groups.csv' -NoTypeInformation -Encoding UTF8
+	Export-Csv $ExportPath'\Groups.csv' -NoTypeInformation -Encoding UTF8
 }
 
 function ADUsers {
-    Get-ADUser -Filter * -Properties * -SearchBase "DC=domain,DC=local" | `
-    Select-Object Name,SamAccountName,EmailAddress,DistinguishedName,Company,Enabled,SID,Country,co,Manager,`
+	Get-ADUser -Filter * -Properties * -SearchBase "DC=domain,DC=local" | `
+	Select-Object Name,SamAccountName,EmailAddress,DistinguishedName,Company,Enabled,SID,Country,co,Manager,`
 	Department,Description,Office,OfficePhone,LastLogonDate,whenCreated,whenChanged,PasswordNeverExpires,`
 	@{Name="ExpirationDate";Expression={[DateTime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}},`
 	@{Name="pwdLastSet";Expression={[DateTime]::FromFileTime($_.pwdLastSet)}},`
 	@{Name='MemberOf';Expression= {($_.MemberOf | % {(Get-ADObject $_).Name}) -join ";"}} | `
-    Export-Csv $ExportPath'\Users.csv' -NoTypeInformation -Encoding UTF8
+	Export-Csv $ExportPath'\Users.csv' -NoTypeInformation -Encoding UTF8
 }
 
 if ($ADComputers) {ADComputers}
--- 21.ExportAll-ADObjects/README.md
+++ 21.ExportAll-ADObjects/README.md
@@ -1,11 +1,11 @@
-# ExportAll-ObjectsAD
+# ExportAll-ADObjects
 #### Export all Active Directory objects in csv format (computers, groups and users).
 
 Set with a switch parameter whether or not to export Computers, Groups or Users object types. The export path is mandatory
 
 Example: Export all the users and groups in the directory
 ```
-.\ExportAll-ObjectsAD.ps1 -ADGroups -ADUsers -ExportPath "C:\Users\adrian\Desktop\ExportAD"
+.\ExportAll-ADObjects.ps1 -ADGroups -ADUsers -ExportPath "C:\Users\adrian\Desktop\ExportAD"
 ```
 
 - *Member, MemberOf*: Expand all members and members of users or groups separated by semicolon (;) and show only their name.
