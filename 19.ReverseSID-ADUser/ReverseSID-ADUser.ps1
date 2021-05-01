761c5cae55ebb5649f4f973decd5650f139eccc1
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-05-01 20:00:15 +0200

    ReverseSID-ADUser

--- 19.ReverseSID-ADUser/ReverseSID-ADUser.ps1
+++ 19.ReverseSID-ADUser/ReverseSID-ADUser.ps1
@@ -1,5 +1,5 @@
 Import-Module ActiveDirectory
 
 $objSID = New-Object System.Security.Principal.SecurityIdentifier ("SIDtoSamAccountName")
-$objUser = $objSID.Translate( [System.Security.Principal.NTAccount])
+$objUser = $objSID.Translate([System.Security.Principal.NTAccount])
 $objUser.Value
