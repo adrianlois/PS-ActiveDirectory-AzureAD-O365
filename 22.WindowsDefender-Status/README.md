cd85dd61073b32315babe10dddf7fcc706cd362f
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-09-23 00:57:38 +0200

    WindowsDefender-Status

---
+++ 22.WindowsDefender-Status/README.md
@@ -0,0 +1,7 @@
+# WindowsDefender-Status
+
+Check the status of the Windows Defender service and components and disable it for security testing to prevent threat detection.
+
+References:
+- https://docs.microsoft.com/en-us/powershell/module/defender/get-mpcomputerstatus?view=windowsserver2019-ps
+- https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-if?view=powershell-7.1
---
+++ 22.WindowsDefender-Status/WindowsDenfender-Status.ps1
@@ -0,0 +1,24 @@
+$defenderOptions = Get-MpComputerStatus
+
+	if ([string]::IsNullOrEmpty($defenderOptions)) {
+		Write-Host "Windows Defender not running on the server:" $env:computername -foregroundcolor "Yellow"
+	} 
+	else {
+		Write-Host 'Windows Defender is active on the server:' $env:computername -foregroundcolor "Cyan"
+		Write-Host ''
+		Write-Host '	Windows Defender enabled?' $defenderOptions.AntivirusEnabled
+		Write-Host '	Windows Defender service enabled?' $defenderOptions.AMServiceEnabled
+		Write-Host '	Windows Defender Antispware enabled?' $defenderOptions.AntispywareEnabled
+		Write-Host '	Windows Defender component OnAccessProtection enabled?' $defenderOptions.OnAccessProtectionEnabled
+		Write-Host '	Windows Defender component RealTimeProtection enabled?' $defenderOptions.RealTimeProtectionEnabled
+	}
+
+$defenderDisable = Read-Host 'Disable Windows Defender on the server? (Y/N)' $env:computername -foregroundcolor "Cyan"
+
+	if ( $defenderDisable -ieq 'y' ) {
+		Uninstall-WindowsFeature -Name Windows-Defender
+		Write-host "Windows Defender is disabled on the server:" $env:computername -foregroundcolor "Green"
+	} 
+	else {
+		Write-host "Windows Defender is still running on the server:" $env:computername -foregroundcolor "Green"
+	}
