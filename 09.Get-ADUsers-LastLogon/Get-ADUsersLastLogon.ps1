df6ce93a6020ce066ed5340973eabe9dcf03ea09
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2022-01-04 14:06:20 +0100

    Get-ADUsersLastLogon.ps1

--- 09.Get-ADUsers-LastLogon/Get-ADUsersLastLogon.ps1
+++ 09.Get-ADUsers-LastLogon/Get-ADUsersLastLogon.ps1
@@ -1,35 +1,28 @@
 Import-Module ActiveDirectory
-
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
 
-function Get-ADUsersLastLogon()
-{
+function Get-ADUsersLastLogon {
   $dcs = Get-ADDomainController -Filter {Name -like "*"}
   $users = Get-ADUser -Filter *
   $time = 0
   $exportFilePath = "%USERPROFILE%\Desktop\ADUsersLastLogon.csv"
   $columns = "Name,Username,Enabled,DateTime,DistinguishedName"
-
+  
   Out-File -FilePath $exportFilePath -Force -InputObject $columns
 
-  foreach($user in $users)
-  {
-    foreach($dc in $dcs)
-    {
+  foreach($user in $users) {
+    foreach($dc in $dcs) {
       $hostname = $dc.HostName
       $currentUser = Get-ADUser $user.SamAccountName | Get-ADObject -Server $hostname -Properties LastLogon
 
-      if($currentUser.LastLogon -gt $time) 
-      {
+      if($currentUser.LastLogon -gt $time) {
         $time = $currentUser.LastLogon
       }
     }
 
     $dt = [DateTime]::FromFileTime($time)
     $row = $user.Name+","+$user.SamAccountName+","+$user.Enabled+","+$dt+","+$user.DistinguishedName
-
     Out-File -FilePath $exportFilePath -Append -NoClobber -InputObject $row
-
     $time = 0
   }
 }
