a7cdca0a1a4bce3e3b98a45a7f3c30e7d7093417
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2022-07-22 16:38:26 +0200

    Get-Properties-By-Email

--- 25.Get-Properties-By-Email/Get-Properties-By-Email.ps1
+++ 25.Get-Properties-By-Email/Get-Properties-By-Email.ps1
@@ -1,9 +1,7 @@
-﻿Import-Module ActiveDirectory
+Import-Module ActiveDirectory
 
 $Emails = "user1@email.com","user2@email.com","user3@email.com","user4@email.com"
 
 Foreach ($Email in $Emails) {
-
     Get-ADUser -Filter {EmailAddress -eq $Email} -Properties * | Select-Object EmailAddress, PasswordNeverExpires, Enabled | Export-Csv "Emails.csv" -NoTypeInformation -Append
-
 }
