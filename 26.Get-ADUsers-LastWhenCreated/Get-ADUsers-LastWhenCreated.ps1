692fc2fd0ad8f1aa0dc011a4c7bf0af2711df5d8
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2022-08-11 12:01:13 +0200

    26.Get-ADUsers-LastWhenCreated

--- 26.Get-ADUsers-LastWhenCreated/Get-ADUsers-LastWhenCreated.ps1
+++ 26.Get-ADUsers-LastWhenCreated/Get-ADUsers-LastWhenCreated.ps1
@@ -1,2 +1,2 @@
 $lastDays = ((Get-Date).AddDays(-30)).Date
-Get-ADUser -Filter {whenCreated -ge $lastDays} -Properties whenCreated | Select-Object Name, whenCreated | Sort-Object whenCreated | findstr /i "Pepe"
+Get-ADUser -Filter {whenCreated -ge $lastDays} -Properties whenCreated | Select-Object Name, whenCreated | Sort-Object whenCreated | findstr /i "NameUser"
