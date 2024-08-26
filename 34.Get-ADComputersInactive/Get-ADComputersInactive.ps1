d8e7fa0ae724d875d733fd455719d07e3c73879f
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2024-08-26 10:42:37 +0200

    Update Get-ADComputersInactive.ps1

--- 34.Get-ADComputersInactive/Get-ADComputersInactive.ps1
+++ 34.Get-ADComputersInactive/Get-ADComputersInactive.ps1
@@ -1,3 +1,3 @@
 Get-ADComputer -filter * -Proper Name, DisplayName, SamAccountName, lastlogondate, PasswordLastSet |`
-ft Name, DisplayName, SamAccountName, lastlogondate, PasswordLastSet |`
+Format-Table Name, DisplayName, SamAccountName, lastlogondate, PasswordLastSet |`
 Export-Csv -Path "%USERPROFILE%\Desktop" -Encoding UTF8 -NoTypeInformation
